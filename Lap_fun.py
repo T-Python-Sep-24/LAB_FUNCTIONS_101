1aa63feda83029afc860f38c4c204c35b1eb445c
SalehBinHamed <Saleh.Alrashidi.H@gmail.com>
2024-10-04 00:36:41 +0300

    done

--- Lap_fun.py
+++ Lap_fun.py
@@ -1,8 +1,11 @@
-def mesi(i):
+def mesi():
     '''this fun is solstion of our task in fun101... '''
     lis=[5,4,3,2,1]
-    for i in range(1,i,1):
+    for i in range(1,6,1):
         print(lis)
         lis.pop(0)
-mesi(6)
-print(mesi.__doc__)
+
+messi=mesi()
+print(mesi.__doc__, messi)
+
+
