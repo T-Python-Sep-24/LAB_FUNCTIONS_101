50b6d9b850debc22d05c4f2bc5154271c876ab47
Alhanouf <hnno3040@gmail.com>
2024-10-04 16:26:53 +0300

    added the documentation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def name(n):
+def name(n: int):
+    '''
+    the function takes the parameters in integer then print thre given integer down to 1.
+    '''
     for i in range(n, 0, -1):
         for j in range(i, 0, -1):
                print(j, end=' ') # 'end' to print the numbers at the same line
