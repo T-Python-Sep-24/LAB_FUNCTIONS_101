e1bdcc9393e465e94517d41eb4bad2ab5880a64c
Mohamed <sirexplq@gmail.com>
2024-10-06 11:18:02 +0300

    solved

--- frst.py
+++ frst.py
@@ -1,6 +1,11 @@
 def new_func(i:int):
+    """
+    this is a function take one parameter then print the out the number in reverse
+
+    """
     for x in range(i,0,-1):
         for y in range(x,0,-1):
             print(y, end=' ')
         print()
 new_func(i=5)
+print(new_func.__doc__)
