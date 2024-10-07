b4df35ce7d68b02fde77fe6077647675b8a390ed
AR <aarokan@gmail.com>
2024-10-07 18:32:12 +0300

    add functions documentation

--- bonus.py
+++ bonus.py
@@ -1,5 +1,18 @@
 # define the function
 def myfunction(n: int) -> str:
+    """
+    prints out a formatted pattern like the following (n = 3):
+    3 2 1
+    2 1
+    1
+    
+    Args:
+        n (int): a number as start for printing the pattern
+
+    Returns:
+        str: a formated pattern as a string
+
+    """
     pattern: str = ""
     for i in range(n, 0, -1):
         for j in range(i, 0, -1):
--- functions_101.py
+++ functions_101.py
@@ -1,5 +1,18 @@
 # define the function
 def myfunction(n: int):
+    """
+    prints out a formatted pattern like the following (n = 3):
+    3 2 1
+    2 1
+    1
+    
+    Args:
+        n (int): a number as start for printing the pattern
+
+    Returns:
+        None: no returned value
+
+    """
     for i in range(n, 0, -1):
         for j in range(i, 0, -1):
             print(j, end="")
